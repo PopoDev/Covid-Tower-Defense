651e136a327ce7ce722fed6cf4ac078ef078f2c8
PopoDev <popocompte@gmail.com>
2021-03-31 18:20:31 +0200

    Adding Road background

---
+++ greenfoot/StraightRoad.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class StraightRoad here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StraightRoad extends Backgrounds
+{
+    /**
+     * Act - do whatever the StraightRoad wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
