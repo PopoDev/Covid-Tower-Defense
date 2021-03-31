b013db6b1cce7c35d63520b75966e3902c2c952f
PopoDev <popocompte@gmail.com>
2021-03-31 18:50:27 +0200

    Adding TurnAtCorner function

--- greenfoot/Mobs.java
+++ greenfoot/Mobs.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
 
 /**
  * Write a description of class Mobs here.
@@ -8,12 +9,57 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Mobs extends Actor
 {
-    /**
-     * Act - do whatever the Mobs wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
+int gridSize = 50;
+    
+    public void turnAtCorner()
     {
-        // Add your action code here.
-    }    
+        int angle = getRotation();
+        if(isTouching(CurvedRoad.class))
+        {
+            int angleRoad = getOneIntersectingObject(CurvedRoad.class).getRotation();
+            
+            if(getX() == getOneIntersectingObject(CurvedRoad.class).getX() &&
+            getY() == getOneIntersectingObject(CurvedRoad.class).getY())
+            {
+                if(!getObjectsInRange(gridSize, StraightRoad.class).isEmpty())
+                {
+                    int x = 0;
+                    int y = 0;
+                    boolean set = false;
+                    
+                    List<StraightRoad> neighbourRight = getObjectsAtOffset(gridSize, 0, StraightRoad.class);
+                    List<StraightRoad> neighbourLeft = getObjectsAtOffset(-gridSize, 0, StraightRoad.class);
+                    List<StraightRoad> neighbourUp = getObjectsAtOffset(0, -gridSize, StraightRoad.class);
+                    List<StraightRoad> neighbourDown = getObjectsAtOffset(0, gridSize, StraightRoad.class);
+                    
+                    if(!neighbourRight.isEmpty() && set == false && angle != 180)
+                    {
+                        x = neighbourRight.get(0).getX();
+                        y = neighbourRight.get(0).getY();
+                        set = true;
+                    }
+                    else if(!neighbourLeft.isEmpty() && set == false && angle != 0)
+                    {
+                        x = neighbourLeft.get(0).getX();
+                        y = neighbourLeft.get(0).getY();
+                        set = true;
+                    }
+                    else if(!neighbourUp.isEmpty() && set == false && angle != 90)
+                    {
+                        x = neighbourUp.get(0).getX();
+                        y = neighbourUp.get(0).getY();
+                        set = true;
+                    }
+                    else if(!neighbourDown.isEmpty() && set == false && angle != -90)
+                    {
+                        x = neighbourDown.get(0).getX();
+                        y = neighbourDown.get(0).getY();
+                        set = true;
+                    }
+                    
+                    turnTowards(x, y);
+                }
+            }
+        }
+    }  
 }
