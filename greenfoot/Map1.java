760a241ae72fb2bc97d1969cdf793aac6944934d
PopoDev <popocompte@gmail.com>
2021-03-31 18:26:37 +0200

    Create Map1 World

---
+++ greenfoot/Map1.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Map 1
+ * 
+ * @author PopoDev
+ * @version 1.0.0
+ */
+public class Map1 extends World
+{
+    public Map1()
+    {    
+        super(800, 800, 1); 
+    }
+}
