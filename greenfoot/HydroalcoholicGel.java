bdb09726ef83a9c280e4dd33af42402e2915b051
PopoDev <popocompte@gmail.com>
2021-04-03 10:58:28 +0200

    Adding HydroalcoholicGel

---
+++ greenfoot/HydroalcoholicGel.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class HydroalcoholicGel here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HydroalcoholicGel extends Disinfectant
+{
+    /**
+     * Act - do whatever the HydroalcoholicGel wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
