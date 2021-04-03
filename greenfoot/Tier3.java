25e78fe273f8bf40c1a6807eed51874217da4d15
PopoDev <popocompte@gmail.com>
2021-04-03 16:15:58 +0200

    Adding Tier3 Virus

---
+++ greenfoot/Tier3.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Tier3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tier3 extends Virus
+{
+    int tier = 3;
+    int speed = 3;
+    int health = 3;
+    
+    public Tier3()
+    {
+        setRotation(90);
+        setTier(tier);
+        setHealth(health);
+    }
+    
+    public void act() 
+    {
+        move(speed);
+        turnAtCorner();
+        changeTier();
+    }
+}
